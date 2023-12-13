b4aa5a0fbdeb8e1d22fded52b9a1ea8ae29ed9ee
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-12-13 03:17:40 +0000

    created: views for login, signup and test

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path
+from . import views
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # path("api-auth/", include('rest_framework.urls'))
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
 ]
